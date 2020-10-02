26e4cd2a0133063c7184054cb1545d81b0b0cae2
sudhanshu.joshi1 <sudhanshu.joshi1@DEL1-LHP-N00763>
2020-10-03 01:28:47 +0530

    Registration functionality Added

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '4982539fcdd7368bf4fcc579fb9b94c0'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -13,6 +14,19 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
 
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()        
+        if user:
+            raise ValidationError('Username already exists in the database. Please choose another username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()        
+        if user :
+            raise ValidationError('Email already exists in the database.')
+
+
+
 
 
 class LoginForm(FlaskForm):    
