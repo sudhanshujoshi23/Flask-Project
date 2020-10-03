f101deca935f8bcfe8e70411de56ce3f5709826c
sudhanshu.joshi1 <sudhanshu.joshi1@DEL1-LHP-N00763>
2020-10-04 00:10:06 +0530

    User Account Modification

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,44 +1,50 @@
+import os
+import secrets
 from flask import render_template, flash, redirect, url_for, request
-from flaskblog import app, db, bcrypt 
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog import app, db, bcrypt
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-
-
+from PIL import Image
 
 
 posts = [
     {
-        'author' : 'Sudhanshu Joshi',
-        'title' : 'First Blog',
-        'content' : 'First Content',
-        'date_posted' : 'April 20, 2020'
+        'author': 'Sudhanshu Joshi',
+        'title': 'First Blog',
+        'content': 'First Content',
+        'date_posted': 'April 20, 2020'
     },
     {
-        'author' : 'Jane Doe',
-        'title' : 'Second Blog',
-        'content' : 'Second Content',
-        'date_posted' : 'April 21, 2020'
+        'author': 'Jane Doe',
+        'title': 'Second Blog',
+        'content': 'Second Content',
+        'date_posted': 'April 21, 2020'
     }
 ]
+
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('Home.html', posts=posts)
 
+
 @app.route('/about')
 def about():
     return render_template('About.html', title='About')
 
-@app.route('/register', methods=['GET','POST'])
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user = User(username=form.username.data,
+                    email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash(f'Your account has been created.', 'success')
@@ -49,7 +55,7 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))    
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
@@ -59,15 +65,43 @@ def login():
             return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash(f'Login Unsuccessful', 'danger')
-                
+
     return render_template('login.html', title='Login', form=form)
 
+
 @app.route('/logout')
-def logout():    
+def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route('/account')
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    fname, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
-def account():      
-    return render_template('account.html', title='Account')
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated.', category='success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file = image_file, form=form)
