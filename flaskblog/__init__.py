97f4bf25d7db71172005a4c000813874f4e10385
sudhanshu.joshi1 <sudhanshu.joshi1@DEL1-LHP-N00763>
2020-10-08 00:22:21 +0530

    Email Reset

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+import os
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '4982539fcdd7368bf4fcc579fb9b94c0'
@@ -11,5 +14,13 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.mail.yahoo.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD')
+mail = Mail(app)
+
+
 
 from flaskblog import routes
